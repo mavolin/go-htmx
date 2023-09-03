171612761c722b485568c4878bf8a691b9df3e5f
mavolin <github@mavolin.co>
2023-09-03 21:51:14 +0200

    docs: improve docs of global functions

--- global.go
+++ global.go
@@ -51,7 +51,28 @@ func (d *LocationData) toHeader() (LocationHeader, error) {
 	return h, nil
 }
 
-// Location sets [ResponseHeaders.Location].
+// Location allows you to do a client-side redirect that does not do a
+// full page reload.
+//
+// This response header can be used to trigger a client side
+// redirection without reloading the whole page.
+// Instead of changing the page’s location it will act like following a
+// hx-boost link, creating a new history entry, issuing an ajax request
+// to the value of the header and pushing the path into history.
+//
+// A sample response would be:
+//
+//	HX-LocationData: /test
+//
+// Which would push the client to test as if the user had clicked on
+// <a href="/test" hx-boost="true">.
+//
+// If you want to redirect to a specific target on the page rather than
+// the default of document.body, you can pass more details along with
+// the event, by setting the remaining Location values.
+//
+// If LocationData.Path is an empty string, the location is not
+// included.
 //
 // Previous values are overwritten.
 func Location(r *http.Request, loc LocationData) error {
@@ -64,13 +85,26 @@ func Location(r *http.Request, loc LocationData) error {
 	return nil
 }
 
-// LocationPath sets the [ResponseHeaders.LocationData.Path], discarding all
-// other values set for the location.
+// LocationPath is a shorthand for Location(r, LocationData{Path: path}).
 func LocationPath(r *http.Request, path URL) {
 	Response(r).Location = LocationHeader{Path: path}
 }
 
-// PushURL sets [ResponseHeaders.PushURL] to the specified same-origin url.
+// PushURL pushes a new url into the history stack:
+//
+// The HX-Push-Url header allows you to push a URL into the browser
+// location history.
+// This creates a new history entry, allowing navigation with the
+// browser’s back and forward buttons.
+// This is similar to the hx-push-url attribute.
+//
+// If present, this header overrides any behavior defined with
+// attributes.
+//
+// To prevent the browser's history from being updated set PushURL to
+// "false".
+//
+// The URL must be from the same origin as the request.
 //
 // Previous values are overwritten.
 func PushURL(r *http.Request, u SameOriginURL) {
@@ -78,7 +112,9 @@ func PushURL(r *http.Request, u SameOriginURL) {
 	resp.PushURL = u
 }
 
-// PreventPushURL sets [ResponseHeaders.PushURL] to false.
+// PreventPushURL sets the HX-PushURL Header to "false".
+//
+// It is equivalent to calling PushURL(r, "false").
 //
 // Previous values are overwritten.
 func PreventPushURL(r *http.Request) {
@@ -86,29 +122,43 @@ func PreventPushURL(r *http.Request) {
 	resp.PushURL = "false"
 }
 
-// Redirect sets [ResponseHeaders.Redirect] to the specified url.
+// Redirect can be used to do a client-side redirect to a new location.
 //
 // Previous values are overwritten.
 func Redirect(r *http.Request, u URL) {
 	Response(r).Redirect = u
 }
 
-// Refresh sets [ResponseHeaders.Refresh] to the specified value.
+// Refresh if set to “true”, will do a full refresh of the page on the
+// client side.
 //
 // Previous values are overwritten.
 func Refresh(r *http.Request, refresh bool) {
 	Response(r).Refresh = refresh
 }
 
-// ReplaceURL sets [ResponseHeaders.ReplaceURL] to the specified same-origin
-// url.
+// ReplaceURL allows you to replace the current URL in the browser
+// location history.
+// This does not create a new history entry; in effect, it removes the
+// previous current URL from the browser’s history.
+// This is similar to the hx-replace-url attribute.
+//
+// If present, this header overrides any behavior defined with
+// attributes.
+//
+// To prevent the browser's current URL from being updated, set
+// ReplaceURL to "false".
+//
+// The URL must be from the same origin as the request.
 //
 // Previous values are overwritten.
 func ReplaceURL(r *http.Request, u SameOriginURL) {
 	Response(r).ReplaceURL = u
 }
 
-// PreventReplaceURL sets [ResponseHeaders.ReplaceURL] to false.
+// PreventReplaceURL sets the HX-ReplaceURL Header to "false".
+//
+// It is equivalent to calling ReplaceURL(r, "false").
 //
 // Previous values are overwritten.
 func PreventReplaceURL(r *http.Request) {
@@ -116,28 +166,30 @@ func PreventReplaceURL(r *http.Request) {
 	resp.ReplaceURL = "false"
 }
 
-// Reswap sets [ResponseHeaders.Reswap] to the passed [SwapStrategy].
+// Reswap allows you to specify how the response will be swapped.
 //
 // Previous values are overwritten.
 func Reswap(r *http.Request, strategy SwapStrategy) {
 	Response(r).Reswap = strategy
 }
 
-// Retarget sets [ResponseHeaders.Retarget] to the passed selector.
+// Retarget is a CSS selector that updates the target of the content
+// update to a different element on the page.
 //
 // Previous values are overwritten.
 func Retarget(r *http.Request, sel Selector) {
 	Response(r).Retarget = sel
 }
 
-// Reselect sets [ResponseHeaders.Reselect] to the passed selector.
+// Reselect is a CSS selector that allows you to choose which part of
+// the response is used to be swapped in.
 //
 // Previous values are overwritten.
 func Reselect(r *http.Request, sel Selector) {
 	Response(r).Reselect = sel
 }
 
-// Trigger adds a new event trigger.
+// Trigger triggers the passed event as soon as the response is received.
 //
 // If a there already is a trigger for that event, it will be overwritten.
 //
@@ -157,9 +209,10 @@ func Trigger(r *http.Request, name Event, data any) error {
 	return nil
 }
 
-// TriggerAfterSettle adds a new event trigger run after settle.
+// TriggerAfterSettle triggers the passed event after the settling step.
 //
-// If a there already is a trigger for that event, it will be overwritten.
+// If a there already is an after-settle trigger for that event, it will be
+// overwritten.
 //
 // An error will only be returned if data can't be marshalled to json.
 // It is guaranteed that TriggerAfterSettle will never return an error for nil
@@ -178,9 +231,10 @@ func TriggerAfterSettle(r *http.Request, name Event, data any) error {
 	return nil
 }
 
-// TriggerAfterSwap adds a new event trigger run after swap.
+// TriggerAfterSwap triggers the passed event after the swap step.
 //
-// If a there already is a trigger for that event, it will be overwritten.
+// If a there already is an after-swap trigger for that event, it will be
+// overwritten.
 //
 // An error will only be returned if data can't be marshalled to json.
 // It is guaranteed that TriggerAfterSwap will never return an error for nil
